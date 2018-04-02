d5161bf36a6664ec974486f66897a94b512a7b22
Oluwafisayo <okefisayo98@gmail.com>
2018-04-02 03:02:26 -0400

    added prime number class

---
+++ PrimeNumber.java
@@ -0,0 +1,85 @@
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JPanel;
+
+
+public class PrimeNumber extends JFrame {
+	private JTextField number;
+	private JButton prime;
+	private JTextField result;
+	private JLabel label;
+
+	private static final int FRAME_WIDTH = 400;
+	private static final int FRAME_LENGTH = 200;
+
+	public PrimeNumber(){
+		setSize(FRAME_WIDTH, FRAME_LENGTH);
+		createTextField();
+	    createButton();
+	    result = new JTextField(10);
+	    result.setEditable(false);
+	    createPanel();
+	}
+	public void createTextField(){
+		label = new JLabel("Enter Number");
+		number = new JTextField(10);
+	}
+	//create an action for the button to perform when clicked
+	class CheckPrime implements ActionListener{
+		public void actionPerformed(ActionEvent e){
+			String value = number.getText();
+			int valueLength = value.length();
+			//check if there is a value that is not integer 
+			for (int i = 0;i < valueLength;i++){
+				if(!Character.isDigit(value.charAt(i))){
+					result.setText("Enter an integer");
+					number.setText("");
+					return; 
+				}
+			}
+		
+			int valueInt = Integer.parseInt(number.getText());
+			if(valueInt == 1){
+				result.setText("False");
+			}
+			else if (valueInt == 2 || valueInt == 3 || valueInt == 5)
+			{
+				result.setText("True");
+			} 
+			else if (valueInt % 2 == 0)
+			{
+				result.setText("False");
+			}
+			else if (valueInt % 3 == 0)
+			{
+				result.setText("False");
+			}
+			else if (valueInt % 5 == 0)
+			{
+				result.setText("False");
+			}
+			else {
+				result.setText("True");
+			}
+		}
+
+	}
+	public void createButton(){
+		prime = new JButton("Prime?");
+		ActionListener listener = new CheckPrime();
+		prime.addActionListener(listener);
+
+	}
+	public void createPanel(){
+		JPanel panel = new JPanel();
+		panel.add(label);
+		panel.add(number);
+		panel.add(prime);
+		panel.add(result);	
+		add(panel);
+	}
+}
